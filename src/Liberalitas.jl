9442cb7e003ddc7c8afbba8ba5b5274df0a71df8
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2024-03-16 19:51:03 +0000

    feat: add class macro and class redefinition

--- src/Liberalitas.jl
+++ src/Liberalitas.jl
@@ -1,5 +1,34 @@
 module Liberalitas
 
-# Write your package code here.
+macro class(name, slots)
+    if isdefined(__module__, name)
+        class_type = getfield(__module__, name)
+        classversion = getfield(__module__, :classversion)
+        version = classversion(class_type) + 1
+    else
+        version = 1
+    end
+
+    struct_name = Symbol(name, "__v", version)
+    struct_head = Expr(:(<:), struct_name, name)
+    struct_class = Expr(:struct, true, struct_head, Expr(:block, slots.args...))
+
+    esc(quote
+        abstract type $name end
+        $struct_class
+
+        global $name(args...) = $struct_name(args...)
+        global classversion(::Type{$name}) = $version
+        global classof(::$name) = $name
+
+        $name
+    end)
+end
+
+macro class(name)
+    esc(:(@class $name ()))
+end
+
+export @class
 
 end
